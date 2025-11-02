9996f93dd1f54e60098cf70f0a603ba05a86e307
zhangyunling <1461428394@qq.com>
2025-11-02 21:00:41 +0800

    docs: 更新Python列表操作示例的注释
    添加关于循环后迭代变量状态的说明
    修正文件末尾的换行符问题

--- 4.列表操作/index.py
+++ 4.列表操作/index.py
@@ -2,5 +2,5 @@
 forInTest = [1,2,3,4];
 for item in forInTest: # 注意语法细节，后面有个冒号，python不依靠大括号来判断代码块，而是依靠缩进
     print(item);# 这里的for in循环得到的直接是item，类似于js中的for of循环
-
+print(item); # 循环结束后，迭代变量依然存在，指向最后一个元素
 
